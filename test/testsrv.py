a2d4a18200e57f0cc3312fb1ea0a404d583ae38b
sys-nyx <nyx@pwnvault.com>
2025-01-03 20:17:24 -0800

    addded request counting to flask test server

--- test/testsrv.py
+++ test/testsrv.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, current_app
 import time
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
-
+setattr(app, "request_count", 0)
 
 @app.route('/', methods = ["GET", "POST", "DELETE", "HEAD", "PUT", "OPTIONS"])
 @app.route('/<path>', methods = ["GET", "POST", "DELETE", "HEAD", "PUT", "OPTIONS"])
 def main(path):
 	time.sleep(1)
+	current_app.request_count += 1
+
+	if current_app.request_count % 100 == 0:
+		print(f"Request Count: {current_app.request_count}")
 	return "Success", 200
 
 app.run()
